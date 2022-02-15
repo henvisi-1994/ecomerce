f0a886bb873d8b07df7241744e0ea3992b00d750
FrankWN29 <44509452+FrankWN29@users.noreply.github.com>
2022-02-15 14:44:15 -0500

    CHATBOT_INTEGRADO

--- ecomerce-front/src/app/modules/home/home.module.ts
+++ ecomerce-front/src/app/modules/home/home.module.ts
@@ -1,5 +1,6 @@
+import { ChatDialogComponent } from './chat-dialog/chat-dialog.component';
 import { SharedModule } from './../../shared/shared.module';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
@@ -7,12 +8,16 @@ import { CarShopComponent } from './car-shop/car-shop.component';
 
 
 
+
 @NgModule({
   declarations: [
     HomeComponent,
     ProductDetailComponent,
-    CarShopComponent
+    CarShopComponent,
+    ChatDialogComponent
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
+
   imports: [
     SharedModule,
     HomeRoutingModule
